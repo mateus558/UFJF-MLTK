f06ae61531e6ff8578f2a2c68a18eea75a650f17
Mateus Coutinho Marim <mateus.marim@ice.ufjf.br>
2021-04-05 23:43:33 -0300

    update?

--- Examples/Ensemble/res_knn_est_acc.cpp
+++ Examples/Ensemble/res_knn_est_acc.cpp
@@ -18,7 +18,7 @@ double estimate_ensemble_acc(Data& data, Model ensemble){
 
 int main(){
     std::vector<std::string> datasets = {"pima.data", "sonar.data", "bupa.data", "wdbc.data", "ionosphere.data", "biodegradetion.csv",
-                                         "vehicle.csv", "seismic-bumps.arff", "ThoraricSurgery.arff"};
+                                         "vehicle.csv", "ThoraricSurgery.arff"};
     bool at_end[10] = {false,false,false,false,false,false,false,true, true,false};
     auto alphas = mltk::linspace<double>(0, 0.7, 8);
     std::vector<std::pair<std::string, bool>> datasets_pair;
@@ -58,7 +58,7 @@ int main(){
             std::for_each(std::execution::par_unseq, orders.begin(), orders.end(), [&max_acc, &log, &_patience, patience,
                                                                                     alpha, &data, dataset_pair](const auto& order){
                if(max_acc == 100.0 || _patience <= 0) return;
-                mltk::ensemble::kNNEnsembleWPDSM<double> knn(data, 5, alpha, order);
+                mltk::ensemble::kNNEnsembleWPDSM<double> knn(data, 7, alpha, order);
                 knn.set_test(true);
                 double acc = estimate_ensemble_acc(data, knn);
 
@@ -92,7 +92,7 @@ int main(){
     file.close();
 
     for(const auto& dataset: log){
-        std::ofstream ofile("max_acc_log/max_acc_k_5_"+dataset.first+"_log.csv");
+        std::ofstream ofile("max_acc_log/max_acc_k_7_"+dataset.first+"_log.csv");
         if(!ofile.is_open()){
             std::cerr << "Error opening log file!" << std::endl;
             return 1;
